1e432b33ae96889d4474957bcd703e60c2ea45c3
agustinAlbornoz <aguswasowski@gmail.com>
2022-04-29 09:41:09 -0300

    arreglando unos nombres de functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Content from './components/Content/Content';
-import MyComponent from './components/StarsRating/StarsRating';
 
 
 
@@ -10,11 +9,10 @@ import MyComponent from './components/StarsRating/StarsRating';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Content/>
-      <Footer/>
-      <MyComponent/>
-      
+      <Navbar />
+      <Content />
+      <Footer />
+
     </div>
   );
 }
--- src/components/StarsRating/StarsRating.jsx
+++ src/components/StarsRating/StarsRating.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import { Rating } from 'react-simple-star-rating'
 
-export default function starRating() {
+export default function MyComponent() {
   const [rating, setRating] = useState(0)
 
 
   const handleRating = (rate: number) => {
     setRating(rate)
-    
+
   }
 
   return (
     <div className='App'>
-      <Rating onClick={handleRating} ratingValue={rating}/>
+      <Rating onClick={handleRating} ratingValue={rating} />
     </div>
   )
 }
